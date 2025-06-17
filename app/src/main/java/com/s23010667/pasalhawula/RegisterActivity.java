6fd3cbeda7e33f534989db95e6aa6a8820df7b66
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-06-17 06:39:59 +0530

    Implemented Java files for all user interfaces and Implemented Java files for all user interfaces and

---
+++ app/src/main/java/com/s23010667/pasalhawula/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.s23010667.pasalhawula;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
