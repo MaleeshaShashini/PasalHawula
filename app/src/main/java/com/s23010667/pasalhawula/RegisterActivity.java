19a53b698327db25b8a3cea94d3de04fc23f5853
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-06-26 21:17:54 +0530

    Fix: RegisterActivity NullPointerException
    The app was crashing on launch due to uninitialized UI elements (register button and login TextView).
    Implemented findViewById to properly link btnRegister and txtAlreadyHaveAccountLogin to their respective views in onCreate.

--- app/src/main/java/com/s23010667/pasalhawula/RegisterActivity.java
+++ app/src/main/java/com/s23010667/pasalhawula/RegisterActivity.java
@@ -4,22 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private TextView txtAlreadyHaveAccountLogin;
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
 
-        Button registerButton = findViewById(R.id.btnRegister);
-
+        registerButton = findViewById(R.id.btnRegister);
+        txtAlreadyHaveAccountLogin = findViewById(R.id.txtAlreadyHaveAccountLogin);
 
         registerButton.setOnClickListener(v -> {
             Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         });
+
+        //Navigation to login page
+        txtAlreadyHaveAccountLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+            }
     }
-}
+
