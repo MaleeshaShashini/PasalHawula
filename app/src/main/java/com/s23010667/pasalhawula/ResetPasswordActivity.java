d88493d599db7bc14153182c2493b483b34fada6
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-06-28 01:35:14 +0530

    Feat: Implement "Send OTP" button navigation
    - Added OnClickListener to the "Send OTP" button on the Reset Password screen.
    - Configured the button to navigate to the `OTPActivity` upon click.

--- app/src/main/java/com/s23010667/pasalhawula/ResetPasswordActivity.java
+++ app/src/main/java/com/s23010667/pasalhawula/ResetPasswordActivity.java
@@ -3,6 +3,7 @@ package com.s23010667.pasalhawula;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View; // Import View class for OnClickListener
+import android.widget.Button;
 import android.widget.ImageView; // Import ImageView class
 import android.os.Bundle;
 
@@ -14,14 +15,23 @@ public class ResetPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
 
+        Button btnSendOTP = findViewById(R.id.btnSendOTP);
         imgBack = findViewById(R.id.imgBack);
         imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create an Intent to start OTP Activity
+                // Create an Intent to start Login Activity
                 Intent intent = new Intent(ResetPasswordActivity.this, MainActivity.class);
                 startActivity(intent); // Start the new activity
             }
+        });
+        btnSendOTP.setOnClickListener(v -> {
+            // Create an Intent to open  OTP Activity
+            Intent intent = new Intent(ResetPasswordActivity.this, OTPActivity.class);
+            // Start HomeActivity
+            startActivity(intent);
+            // Close the Reset Password Activity so the user can't return to it
+            finish();
         });
     }
 }
