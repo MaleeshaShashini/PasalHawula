5024a96a0ce4be9c9828fd9eea2c2cd4880bff5f
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-06-28 01:25:01 +0530

    Feat: Implement Back Navigation to Login Screen
    - Renamed ImageView ID from `btnBack` to `imgBack` in `activity_reset_password.xml` .
    - Configured `imgBack` OnClickListener in `ResetPasswordActivity` to navigate back to `MainActivity` (Login Page).
    - Ensures users can return to the login screen from the password reset flow.

--- app/src/main/java/com/s23010667/pasalhawula/ResetPasswordActivity.java
+++ app/src/main/java/com/s23010667/pasalhawula/ResetPasswordActivity.java
@@ -1,14 +1,27 @@
 package com.s23010667.pasalhawula;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View; // Import View class for OnClickListener
+import android.widget.ImageView; // Import ImageView class
 import android.os.Bundle;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
+    private ImageView imgBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        imgBack = findViewById(R.id.imgBack);
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start OTP Activity
+                Intent intent = new Intent(ResetPasswordActivity.this, MainActivity.class);
+                startActivity(intent); // Start the new activity
+            }
+        });
     }
 }
