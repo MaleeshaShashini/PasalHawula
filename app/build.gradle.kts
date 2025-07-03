eba3ba44f127ce52f040121d5fb7f9fa3bb4e737
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-07-03 22:21:18 +0530

    Add Google Maps and Location Picker integration for selecting location in Add Details activity
    - Integrated Google Maps SDK and Places SDK dependencies
    - Configured API key and permissions in AndroidManifest.xml
    - Created MapSelectionActivity to show map and select location
    - Designed activity_map_selection.xml with SupportMapFragment and a selection button
    - Prepared app for interactive location input via map icon in AddDetailsActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
-    implementation ("androidx.fragment:fragment:1.3.6")
+    implementation ("androidx.fragment:fragment:1.8.8")
+    implementation ("com.google.android.gms:play-services-maps:19.2.0")
+    implementation ("com.google.android.gms:play-services-location:21.3.0")
 }
