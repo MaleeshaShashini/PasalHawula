9df072f7e8ac829c514d0b775c49dc64b157e566
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-07-11 00:08:48 +0530

    Implement back navigation in EditProfileActivity
    - Initialized imgBack ImageView
    - Set OnClickListener to navigate back to ProfileActivity

--- app/src/main/java/com/s23010667/pasalhawula/EditProfileActivity.java
+++ app/src/main/java/com/s23010667/pasalhawula/EditProfileActivity.java
@@ -2,13 +2,28 @@ package com.s23010667.pasalhawula;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
-
+    private ImageView imgBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        imgBack = findViewById(R.id.imgBack);
+
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start Profile Activity
+                Intent intent = new Intent(EditProfileActivity.this, ProfileActivity.class);
+                startActivity(intent); // Start the new activity
+            }
+        });
     }
 }
