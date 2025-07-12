3080c9a1e51197f95b7f6154ac0dd69c2d500ed8
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-07-12 22:06:07 +0530

    Implement btnBack navigation in ResetPasswordActivity
    - Set OnClickListener to navigate to ResetPasswordActivity when click on btnBack

--- app/src/main/java/com/s23010667/pasalhawula/OTPActivity.java
+++ app/src/main/java/com/s23010667/pasalhawula/OTPActivity.java
@@ -2,12 +2,15 @@ package com.s23010667.pasalhawula;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ImageView;
 
 
 public class OTPActivity extends AppCompatActivity {
 
+    private ImageView imgBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +18,16 @@ public class OTPActivity extends AppCompatActivity {
 
         // Find the Login button by its ID
         Button btnLogin = findViewById(R.id.btnLogin);
+        imgBack = findViewById(R.id.imgBack);
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start Home Activity
+                Intent intent = new Intent(OTPActivity.this, ResetPasswordActivity.class);
+                startActivity(intent); // Start the new activity
+            }
+        });
 
         // Set a click listener for the Login button
         btnLogin.setOnClickListener(v -> {
