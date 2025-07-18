0504efd60ad44da82b97e424d89430c867a80bc0
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-07-18 10:02:23 +0530

    feat: Initialize EditText fields for school registration form -Add EditText initializations for school name, contact person, email, phone, password, and confirmation

--- app/src/main/java/com/s23010667/pasalhawula/RegisterActivity.java
+++ app/src/main/java/com/s23010667/pasalhawula/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class RegisterActivity extends AppCompatActivity {
     private TextView txtAlreadyHaveAccountLogin;
     private Button registerButton;
     DatabaseHelper myDb;
+    EditText editSchoolName, editContactPerson, editEmail, editPhone, editPassword, editPasswordConfirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,13 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.btnRegister);
         txtAlreadyHaveAccountLogin = findViewById(R.id.txtAlreadyHaveAccountLogin);
         myDb = new DatabaseHelper(this);
+
+        editSchoolName = findViewById(R.id.editSchoolName);
+        editContactPerson = findViewById(R.id.editContactPerson);
+        editEmail = findViewById(R.id.editEmail);
+        editPhone = findViewById(R.id.editPhone);
+        editPassword = findViewById(R.id.editPassword);
+        editPasswordConfirm = findViewById(R.id.editPasswordConfirm);
 
         registerButton.setOnClickListener(v -> {
             Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
